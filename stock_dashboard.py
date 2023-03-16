99000850392f5ee84a1a7d31a26c62669e35c51a
sod-efi <127058935+sod-efi@users.noreply.github.com>
2023-03-16 00:13:31 -0700

    Update stock_dashboard.py

--- stock_dashboard.py
+++ stock_dashboard.py
@@ -1,41 +1,48 @@
 import os
-os.environ['NUMEXPR_NUM_THREADS'] = '1'
-
 import streamlit as st
-import yfinance as yf
+import pandas_datareader as pdr
 import pandas as pd
 import matplotlib.pyplot as plt
+import requests
 
-def get_market_cap_to_assets_ratio(stock, start_date, end_date):
-    df = yf.download(stock, start=start_date, end=end_date, progress=False, auto_adjust=True)
-    if not hasattr(df.index, 'tz_localize'):
-        df.index = pd.to_datetime(df.index, utc=True)
-    df['Market Cap'] = df['Close'] * df['Volume']
-    df['Total Assets'] = df['Market Cap'] / 1.5  # You need to replace this with a real API to fetch the total assets
+def get_financial_data(stock, api_key):
+    url = f"https://financialmodelingprep.com/api/v3/profile/{stock}?apikey={d24086a7252e0ce8946a897be9845c8e}"
+    response = requests.get(url)
+    data = response.json()
+    if data:
+        return data[0]['mktCap'], data[0]['totalAssets']
+    else:
+        raise ValueError(f"No data found for {stock}")
+
+def get_market_cap_to_assets_ratio(stock, start_date, end_date, api_key):
+    market_cap, total_assets = get_financial_data(stock, api_key)
+    df = pdr.get_data_yahoo(stock, start=start_date, end=end_date)
+    df['Market Cap'] = market_cap
+    df['Total Assets'] = total_assets
     df['MC/TA Ratio'] = df['Market Cap'] / df['Total Assets']
     df = df.resample('Q').mean()
     return df['MC/TA Ratio']
 
-st.title("Stock Dashboard: Market Cap / Total Assets Ratio")
+st.title("Stock Market Cap / Total Assets Ratio Dashboard")
 
-stock_input = st.text_area("Enter a list of up to 100 stock symbols separated by commas:", max_chars=1000)
-stocks = [s.strip() for s in stock_input.split(',') if s.strip()]
+api_key = st.text_input("Enter your Financial Modeling Prep API Key:", type="password")
 
-start_date = st.date_input("Start date", value=pd.to_datetime("2010-01-01"))
-end_date = st.date_input("End date", value=pd.to_datetime("today"))
+stocks = st.text_input("Enter up to 100 stock symbols separated by commas:", "AAPL,MSFT,GOOG")
+stocks = stocks.split(',')
+
+start_date = st.date_input("Start Date:", value=pd.to_datetime('2020-01-01'))
+end_date = st.date_input("End Date:", value=pd.to_datetime('2021-12-31'))
 
 if st.button("Show Dashboard"):
-    if len(stocks) > 100:
-        st.warning("Please enter a maximum of 100 stock symbols.")
-    else:
-        for stock in stocks:
-            st.subheader(f"{stock} Market Cap / Total Assets Ratio")
-            try:
-                mc_ta_ratio = get_market_cap_to_assets_ratio(stock, start_date, end_date)
-                plt.figure(figsize=(10, 5))
-                plt.plot(mc_ta_ratio)
-                plt.xlabel("Date")
-                plt.ylabel("MC/TA Ratio")
-                st.pyplot(plt)
-            except Exception as e:
-                st.error(f"Error fetching data for {stock}: {e}")
+    for stock in stocks:
+        st.subheader(f"{stock} Market Cap / Total Assets Ratio")
+        try:
+            mc_ta_ratio = get_market_cap_to_assets_ratio(stock, start_date, end_date, api_key)
+            plt.figure(figsize=(12, 6))
+            plt.plot(mc_ta_ratio)
+            plt.xlabel("Date")
+            plt.ylabel("MC/TA Ratio")
+            plt.title(f"{stock} Market Cap / Total Assets Ratio")
+            st.pyplot(plt)
+        except Exception as e:
+            st.error(f"Error fetching data for {stock}: {e}")
